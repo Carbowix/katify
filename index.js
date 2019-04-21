ca7ccefdabad4d3aafa7aae94a85ae9942c9bdc6
Carbowix <killeryoyo01@gmail.com>
2019-04-22 01:23:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const request = require('https');
-const cheerio = require('cheerio')
-const catStuff = require('./catStuff.json');
-const catFaces = require('./catFaces.json');
-const randomImageUrl = 'https://api.thecatapi.com/api/images/get?format=xml'
+const request = require("https");
+const cheerio = require("cheerio");
+const catStuff = require("./catStuff.json");
+const catFaces = require("./catFaces.json");
+const randomImageUrl = "https://api.thecatapi.com/api/images/get?format=xml";
 const randomCatFactUrl = "https://catfact.ninja/";
 const catNameUrl = "https://www.findcatnames.com/";
-const randomSelection = choices => choices[Math.floor(Math.random() * choices.length)];
+const randomSelection = (choices) => choices[Math.floor(Math.random() * choices.length)];
 
 module.exports = {
     randomCat: () => {
@@ -25,7 +25,7 @@ module.exports = {
     },
 
     randomCatFace: () => { // Credits to https://github.com/melaniecebula/cat-ascii-faces
-        return randomSelection(catFaces['faces']);
+        return randomSelection(catFaces["faces"]);
     },
 
     randomCatFact: () => {
@@ -80,17 +80,17 @@ module.exports = {
         return new Promise((resolve, reject) => {
             let availableC = [];
             if (!catStuff["gender"].includes(gender.toLowerCase())) {
-                reject(new TypeError(`Invalid gender, Available Genders: ${genders.map(g => g)}`));
+                reject(new TypeError(`Invalid gender, Available Genders: ${catStuff["gender"].map((g) => g)}`));
             }
 
             if (!characteristics || characteristics.length < 1) {
-                reject(new TypeError(`Charateristics are required to find a name, Available Characteristics: ${catStuff["characteristics"].map(c => c)}, and Available Themes: ${catStuff["themes"].map(t => t)}`))
+                reject(new TypeError(`Charateristics are required to find a name, Available Characteristics: ${catStuff["characteristics"].map((c) => c)}, and Available Themes: ${catStuff["themes"].map(t => t)}`));
             }
 
             characteristics.forEach(c => {
                 if (!catStuff["characteristics"].includes(c)) {
                     if (!catStuff["themes"].includes(c)) {
-                        console.warn(`Invalid Characteristic ${c} was entered.`);
+                        console.log(`Invalid Characteristic ${c} was entered.`);
                     } else {
                         availableC.push(c);
                     }
@@ -99,19 +99,19 @@ module.exports = {
                 }
             });
             if (availableC.length > 0) {
-                request.get(catNameUrl + "names/?gender=" + gender + "&q=" + availableC.map(c => c).join('-'), res => {
+                request.get(catNameUrl + "names/?gender=" + gender + "&q=" + availableC.map((c) => c).join('-'), (res) => {
                     if (res.statusCode === 200) {
                         res.setEncoding("utf-8");
                         res.on("data", (d) => {
                             let names = [];
                             const $ = cheerio.load(d);
-                            $('label').each(function (i, e) {
+                            $("label").each(function (i, e) {
                                 names.push($(e).text());
                             });
                             resolve(names);
                         });
                     } else {
-                        reject(new Error('No response for CAT NAME API, try again later.'))
+                        reject(new Error('No response for CAT NAME API, try again later.'));
                     }
                 });
             } else {
